546e2e9c11d40c0a94a8ae4b98250399011aa391
amr <mabroukiamr04@gmail.com>
2024-12-07 20:54:51 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+setup(
+    name='REALMADRID',
+    version='0.1',
+    description='A demonstration package with version submodules',
+    url='',
+    author='MABROUKI Amr',
+    author_email='mabroukiamr04@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    zip_safe=False
+)
---
+++ test.py
@@ -0,0 +1,7 @@
+from REALMADRID import Real
+
+Mbappe = Real("ancelotti", "perez")
+print(Mbappe.get_manager())  # Output: ancelotti
+Mbappe.get_president()  # Output: perez
+Mbappe.change_manager("xabi alonso")
+Mbappe.team_logo()
